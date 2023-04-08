Fix: Correcting the log-likelihood of the negative binomial
Reparameterizing the log-likelihood of the negative binomial so
that the first parameter of the negative binomial probability mass
function is the mean of the distribution instead of the number of
successes until the experiment is stopped.

src/TranscriptDecomPy/inla_TD.py
@@ -14,8 +14,7 @@ def log_likelihood_neg_binom(obs: int, x: int, theta: float) -> float:
 
     Args:
         obs (int): observation of which to calculate the likelihood.
-        x (int): number of successes until the experiment is stopped in the 
-            negative binomial distribution.
+        x (int): mean of the negative binomial distribution.
         theta (float): success probability in experiment in the negative 
             binomial distribution.
 
@@ -29,7 +28,8 @@ def log_likelihood_neg_binom(obs: int, x: int, theta: float) -> float:
         0 <= theta <= 1
     """
 
-    return np.sum(nbinom.logpmf(obs, x, theta), 1)
+    n = x * (theta/(1-theta))
+    return np.sum(nbinom.logpmf(obs, n, theta), 1)
 
 
 def central_differences(objective: Callable, x: float, h: float) -> float: