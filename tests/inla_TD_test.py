9f6bb622a71a7b64216f84cf71ceefbfb38a7fdf
JP2511 <joaopedro.de.mendonca@gmail.com>
2023-04-12 12:15:47 +0100

    test: Tests for Central Difference functions
    Created tests for the functions:
    -expand_increment_axis
    -fst_order_central_differences
    -snd_order_central_differences
    
     The main aim of these tests was to see if the broadcasting was
    working as intended in these functions.

--- tests/inla_TD_test.py
+++ tests/inla_TD_test.py
@@ -6,19 +6,20 @@ from TranscriptDecomPy import inla_TD as tdp
 ###############################################################################
 
 import unittest
+import functools
 
 import numpy as np
 
-from scipy.stats import halfnorm, nbinom
+from scipy.stats import halfnorm, nbinom, multivariate_normal
 
-###############################################################################
 
+# ###############################################################################
 
 class TestLikelihood(unittest.TestCase):
 
-    # --------------------------------------- #
-    #  tests for the function getting_bags    #
-    # --------------------------------------- #
+    # ----------------------------------------------------- #
+    #  tests for the function Negative Binomial Likelihood  #
+    # ----------------------------------------------------- #
 
     def test_likelihood_with_different_dist(self):
         
@@ -36,23 +37,148 @@ class TestLikelihood(unittest.TestCase):
 
 
 
-unittest.main()
+class TestDerivativesApprox(unittest.TestCase):
 
+    # ---------------------------------------------- #
+    #  tests for the function expand_increment_axis  #
+    # ---------------------------------------------- #
 
+    def test_expand_increment_axis(self):
 
-def read_data(name: str) -> np.ndarray:
-
-    with open(name, 'r', encoding='utf8') as datafile:
-        data = []
-        for line in datafile.read().splitlines()[1:]:
-            data.append(line.split(",")[1:])
-        return np.array(data, dtype=int)
+        result = tdp.expand_increment_axis(3, 3)
+        expected_result = [[[3, 0, 0]],
+                            [[0, 3, 0]],
+                            [[0, 0, 3]]]
+        
+        return np.testing.assert_array_equal(result, expected_result)
     
 
-# data = read_data("cage_data_test.csv")
-# x = np.full(109, 10000)[np.newaxis, :]
+    # ------------------------------------------------------ #
+    #  tests for the function fst_order_central_differences  #
+    # ------------------------------------------------------ #
+    
 
-# import time
-# start = time.time()
-# print(np.sum(individual_log_neg_binom_likelihood(data, x, np.array([0.2])), 1))
-# print(f"It took {time.time() - start}")
+    def test_fst_order_cdf_1_rep_mult_out(self):
+        """Tests the function giving it only one observation and making the
+        objective function return an array."""
+
+        n_feat = 4
+        x = np.arange(n_feat)
+        h = 0.2
+        increment = tdp.expand_increment_axis(n_feat, h)
+        test_f = lambda z: z
+
+        result = tdp.fst_order_central_differences(test_f, x, increment, h)
+        expected_result = np.eye(n_feat)[:, np.newaxis, :]
+
+        return np.testing.assert_array_almost_equal(result, expected_result)
+    
+
+    def test_fst_order_cdf_5_rep_mult_out(self):
+        """Tests the function giving it 5 observations and making the objective 
+        function return an array."""
+
+        n_feat = 2
+        x = np.array([6,2])
+        h = 0.1
+        increment = tdp.expand_increment_axis(n_feat, h)
+        obs = np.ones((5,2))
+        test_f = lambda z: obs + z
+
+        result = tdp.fst_order_central_differences(test_f, x, increment, h)
+        expected_result = np.repeat(np.eye(2)[:, np.newaxis, :], 5, 1)
+
+        return np.testing.assert_almost_equal(result, expected_result)
+    
+
+    def test_fst_order_cdf_1_rep_1_out(self):
+        """Tests the function giving it only 1 observation and making the 
+        objective function return a single value based on an array."""
+
+        n_feat = 4
+        x = np.arange(n_feat)
+        h = 0.2
+        increment = tdp.expand_increment_axis(n_feat, h)
+        test_f = lambda z: np.sum(z, 2)
+
+        result = tdp.fst_order_central_differences(test_f, x, increment, h)
+        expected_result = np.ones(n_feat)[:, np.newaxis]
+
+        return np.testing.assert_array_almost_equal(result, expected_result)
+
+
+    def test_fst_order_cdf_5_rep_1_out(self):
+        """Tests the function giving it 5 observations and making the objective 
+        function return a single value based on an array."""
+        
+        n_feat = 2
+        x = np.array([6,2])
+        h = 0.1
+        increment = tdp.expand_increment_axis(n_feat, h)
+        obs = np.array([np.arange(5), np.ones(5)]).T
+        test_f = lambda z: np.sum(obs + z, 2)
+
+        result = tdp.fst_order_central_differences(test_f, x, increment, h)
+        expected_result = np.ones((n_feat, 5))
+
+        return np.testing.assert_almost_equal(result, expected_result)
+    
+
+    # ------------------------------------------------------ #
+    #  tests for the function snd_order_central_differences  #
+    # ------------------------------------------------------ #
+
+    def test_snd_order_cdf_1_rep_mult_out(self):
+        """Tests the function giving it only one observation and making the
+        objective function return an array."""
+
+        n_feat = 4
+        x = np.arange(n_feat)
+        h = 1
+        increment = tdp.expand_increment_axis(n_feat, h)
+        test_f = lambda z: z**2
+
+        result = tdp.snd_order_central_differences(test_f, x, increment, h)
+        expected_result = 2 * np.eye(n_feat)[:, np.newaxis, :]
+
+        return np.testing.assert_array_almost_equal(result, expected_result)
+    
+
+    def test_snd_order_cdf_3_rep_mult_out(self):
+        """Tests the function giving it 3 observations and making the objective 
+        function return an array."""
+
+        n_feat = 4
+        x = np.arange(4)
+        h = 1
+        increment = tdp.expand_increment_axis(n_feat, h)
+        obs = np.arange(12).reshape((3, n_feat))
+        test_f = lambda z: (obs + z)**2
+
+        result = tdp.snd_order_central_differences(test_f, x, increment, h)
+        expected_result = 2 * np.eye(n_feat, dtype=float)[:, np.newaxis, :]
+        expected_result = np.repeat(expected_result, 3, 1)
+
+        return np.testing.assert_almost_equal(result, expected_result)
+    
+
+    def test_snd_order_cdf_1_rep_1_out(self):
+        """Tests the function giving it only 1 observation and making the 
+        objective function return a single value based on an array."""
+        
+        n_feat = 4
+        x = np.arange(n_feat)
+        h = 1
+        increment = tdp.expand_increment_axis(n_feat, h)
+        test_f = lambda z: np.sum(z**2, len(z.shape)-1)
+
+        result = tdp.snd_order_central_differences(test_f, x, increment, h)
+        expected_result = np.full((n_feat, 1), 2)
+
+        return np.testing.assert_array_almost_equal(result, expected_result)
+
+
+
+###############################################################################
+
+unittest.main()
