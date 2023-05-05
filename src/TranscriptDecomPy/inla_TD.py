e5ee24f0830472eec80695e15eeaaecd0686db59
JP2511 <joaopedro.de.mendonca@gmail.com>
2023-05-05 11:21:07 +0200

    Feat: Restructuring and Adding Functions to Allow a Parameterized GMRF
    Additions:
    -build_gmrf_precision_mat was created in order to facilitate the
    creation of the precision matrix of a model that corresponds to
    the sum of an intercept, a fixed-effects component and a first
    -order Random-Walk, with parameters that can be chosen when
    using this function.
    
    -gmrf_density was created in order to have a function that
    calculates the density of the gmrf (which corresponds to a
    Multivariate Normal Distribution).
    
    -create_gmrf_density_func was created in order to not have to
    not have to recompute the determinant of the same precision
    matrix multiple times. This generates a density function for a
    given precision matrix.
    
    Alterations:
    -log_likelihood_neg_binom was altered in order to accomodate the
    fact that although the gmrf corresponds to a vector of 2n+1
    elements only the last n are used to generated the observations.
    
    -newton_raphson_method was altered in order to incorporate the
    Cholesky Decomposition has we can make the assumption of
    symmetric positive-definite matrices. Some of the arguments were
    also adjusted to the new definition of GMRF that has mean 0.
    
    -approx_marg_post_of_theta was altered in order to accomodate the
    changes made to the rest of the code.

--- src/TranscriptDecomPy/inla_TD.py
+++ src/TranscriptDecomPy/inla_TD.py
@@ -1,8 +1,19 @@
+import os
+os.environ["CHOLMOD_USE_GPU"] = "1"
+
+from sksparse.cholmod import cholesky
+
+###############################################################################
+
+
 import numpy as np
+
+from functools import partial
 
 from scipy import linalg, sparse
 from scipy.stats import nbinom
 from scipy.optimize import minimize
+
 
 from typing import Callable
 
@@ -11,7 +22,7 @@ from typing import Callable
 
 
 def log_likelihood_neg_binom(x: np.ndarray, obs: np.ndarray, 
-                                theta: float) -> np.ndarray:
+                                theta_y: float) -> np.ndarray:
     """Calculates the log likelihood of the negative binomial distribution. It 
     is assumed that a point in this distribution is multivariate. As such, the
     log-likelihood of a such a point corresponds to the sum of the 
@@ -20,10 +31,11 @@ def log_likelihood_neg_binom(x: np.ndarray, obs: np.ndarray,
     value obtained corresponds to the sum of the log-likelihood of each point. 
 
     Args:
-        x (np.ndarray): mean of the negative binomial distribution.
+        x (np.ndarray): Gaussian Markov Random Field sample. If it contains n
+            elements, only the last (n-1)//2 elements will be used.
         obs (np.ndarray): observations of which to calculate the probability 
-            mass (pm).
-        theta (float): success probability in experiment in the negative 
+            mass (pmf).
+        theta_y (float): success probability in experiment in the negative 
             binomial distribution.
 
     Returns:
@@ -33,16 +45,103 @@ def log_likelihood_neg_binom(x: np.ndarray, obs: np.ndarray,
     Requires:
         obs_i > 0
         x_i > 0
-        0 <= theta <= 1
+        0 <= theta_y <= 1
     """
     
-    n = x * (theta/(1-theta))
-    pm_for_single_obs = np.sum(nbinom.logpmf(obs, n, theta), -1)
+    n_feat = (x.shape[-1] -1) // 2
+    x = x[:, :, -n_feat:] if len(x.shape) > 1 else x[-n_feat:]
+    
+    n = np.exp(x) * (theta_y/ (1 - theta_y))
+    pm_for_single_obs = np.sum(nbinom.logpmf(obs, n, theta_y), -1)
     
     if len(pm_for_single_obs.shape) > 0:
         return np.sum(pm_for_single_obs, -1)
     
     return pm_for_single_obs
+
+
+def build_gmrf_precision_mat(n_dim: int, theta_intercept: float, 
+                                theta_PD: float,
+                                theta_PI: float) -> sparse.csc_matrix:
+    """Constructs a precision matrix (2 * n_dim + 1) x (2 * n_dim + 1) which 
+    models a gmrf x where:
+        intercept ~ N(0, 1/theta_intercept)
+        PD        ~ N(0, (theta_PD * R)^-1 )
+        PI        ~ N(0, (theta_PI * I)^-1 )
+        
+        eta = intercept + PD + PI
+        x   = (intercept, PD^T, eta^T)^T
+
+        Here, I is the identity matrix of dimentions n_dim x n_dim and R is a
+    first-order Random Walk graph in the form of a matrix n_dim x n_dim.
+
+    Args:
+        n_dim (int): number of dimensions of the observed variable to model.
+        theta_intercept (float): parameter that controls the variance of the
+            intercept of the model.
+        theta_PD (float): parameter that controls the variance/influence of the
+            Random Walk component of the model.
+        theta_PI (float): parameter that controls the variance/influence of the 
+            fixed effects component of the model.
+
+    Returns:
+        sparse.csc_matrix: precision matrix.
+    """
+
+    Q_11 = [ theta_intercept + n_dim * theta_PI ]
+    k_v_1T = np.full(n_dim, theta_PI)
+    k_v_1 = k_v_1T[:, np.newaxis]
+    k_v_I = sparse.diags(np.full(n_dim, theta_PI), format='csr')
+
+    R = sparse.diags(np.full(n_dim, theta_PD*2))
+    R.setdiag(-theta_PD, k=-1)
+    R.setdiag(-theta_PD, k=1)
+    R = sparse.csr_matrix(R)
+
+    fst_block_row = sparse.hstack((Q_11,  k_v_1T    , -k_v_1T), format='csc')
+    snd_block_row = sparse.hstack((k_v_1 , R + k_v_I, -k_v_I ), format='csc')
+    trd_block_row = sparse.hstack((-k_v_1, -k_v_I   ,  k_v_I ), format='csc')
+
+    return sparse.vstack((fst_block_row, snd_block_row, trd_block_row), 
+                            format='csc')
+
+
+
+def gmrf_density(x: np.ndarray, det: float, Q: sparse.csc_matrix) -> float:
+    """Calculates the density of a value using the probability density function
+    of a multivariate normal distribution.
+
+    Args:
+        x (np.ndarray): point used to calculate the density.
+        det (float): determinant of the precision matrix of the multivariate
+            normal distribution used.
+        Q (sparse.csc_matrix): precision matrix of the multivariate normal 
+            distribution used.
+
+    Returns:
+        density (float): density of the point given in the distribution given.
+    """
+    
+    n = x.shape[0]
+    exp_value = -0.5 * (x @ Q @ x[:, np.newaxis])
+    density = ((2 * np.pi)**(-n/2)) * np.sqrt(det) * np.exp(exp_value)
+    return density
+
+
+def create_gmrf_density_func(Q: sparse.csc_matrix) -> Callable:
+    """Generates a function that calculates the density of the GMRF assuming the
+    the precision matrix given.
+
+    Args:
+        Q (sparse.csc_matrix): precision matrix of the GMRF.
+
+    Returns:
+        Callable: function that calculates the density of the GMRF.
+    """
+    
+    det = np.exp(cholesky(Q).logdet())
+
+    return partial(gmrf_density, det=det, Q=Q)
 
 
 ###############################################################################
@@ -164,27 +263,28 @@ def approx_taylor_expansion(objective: Callable, x: np.ndarray,
     return (b, c)
 
 
-def newton_raphson_method(objective: Callable, Q: sparse.dia_matrix, 
-                            mu: np.ndarray, h: float, threshold: float, 
-                            max_iter: int, init_v: np.ndarray) -> tuple:
+def newton_raphson_method(objective: Callable, 
+                            Q: sparse.csc_matrix, 
+                            h: float, 
+                            threshold: float, 
+                            max_iter: int, 
+                            init_v: np.ndarray) -> tuple:
     """Calculates the Gaussian approximation to the full conditional of the 
     GMRF, p(x|y,theta), using a second-order Taylor expansion.
 
         The Gaussian approximation is made by specifically matching the modal 
     configuration and the curvature at the mode.
-        The calculations make use of sparse diagonal matrix linear algebra. 
-    We use this because we make the assumption that the precision matrix of the
-    GMRF is not only sparse but diagonal as well. Since we are only looking at
-    implementing the model for a combination of a first-order Random-Walk and
-    a mixed-effects iid model, the assumption should be valid. It should also be
-    true that precision matrix should only have the main diagonal and the first
-    offset diagonals with nonzero elements.
+        The calculations make use of the Cholesky decomposition. We assume that
+    the precision matrix is symmetric positive-definite and that even after 
+    summing diag(c) it maintains this property. Since this matrix is sparse,
+    the Cholesky decomposition will also be sparse, leading to speed-ups. The
+    Cholesky decomposition is faster than LU decomposition. The Cholesky 
+    decomposition is accelerated using the GPU.
 
     Args:
         objective (Callable): log-likelihood function to use in the 
             approximation.
         Q (sparse.dia_matrix): precision matrix of the GMRF.
-        mu (np.ndarray): mean of the GMRF.
         h (float): step used in the finite differences approximations of the
             derivatives.
         threshold (float): maximum Euclidean-distance required to consider that
@@ -202,8 +302,8 @@ def newton_raphson_method(objective: Callable, Q: sparse.dia_matrix,
     Returns:
         new_x (np.ndarray): mean of the Gaussian approximation to the full 
             conditional of the GMRF.
-        matrix_A (np.ndarray): precision matrix of the Gaussian approximation
-            to the full conditional of the GMRF.
+        matrix_A (float): determinant of the precision matrix of the 
+            Gaussian approximation to the full conditional of the GMRF.
     """
     
     current_x = init_v
@@ -211,23 +311,22 @@ def newton_raphson_method(objective: Callable, Q: sparse.dia_matrix,
         b, c = approx_taylor_expansion(objective, current_x, h)
 
         ## Calculates the mean solving an equation of the type: 
-        ##    matrix_A @ x = result_b
-        matrix_A = Q + sparse.diags(c, format='dia')
-        result_b = Q @ mu + b
+        ##    matrix_A @ x = b
+        matrix_A = Q + sparse.diags(c, format='csc')
+        chol_factor = cholesky(matrix_A)
+        new_x = chol_factor(b)
 
-        new_x = linalg.solve_banded((1,1), matrix_A.data, result_b)
 
-        if linalg.norm(current_x - new_x) < threshold:
-            return (new_x, matrix_A)
+        if linalg.norm(b) < threshold:
+            return (new_x, np.exp(chol_factor.logdet()))
         
         current_x = new_x
     
-    raise Exception("Max iteration achieved and Newton_Raphson method " + 
-                    "did not converge")
+    return (new_x, np.exp(chol_factor.logdet()))
 
 
 
-# ###############################################################################
+###############################################################################
 # calculating p(theta | y)
 
 def general_determinant(A: sparse.dia_matrix, zero: float=1e-5) -> float:
@@ -263,57 +362,46 @@ def general_determinant(A: sparse.dia_matrix, zero: float=1e-5) -> float:
 
 
 def approx_marg_post_of_theta(data_likelihood: Callable,
-                                theta: float,
-                                alpha: np.ndarray,
-                                Q: sparse.dia_matrix,
-                                Q_det: float,
+                                theta: np.ndarray,
+                                gmrf_likelihood: Callable,
                                 theta_dist: Callable,
                                 gaus_approx_mean: np.ndarray,
-                                gaus_approx_Q: sparse.dia_matrix) -> float:
+                                gaus_approx_det: float) -> float:
     """Approximates the marginal posterior of theta, p(theta|y).
-
-        Since we assume that the precision matrix Q of the GRMF or the matrix 
-    Q + diag(c) are singular (not of full-rank), we calculate the general 
-    determinant instead of the common determinant. These calculations assume
-    that these matrices are diagonal (main diagonal and the first offset 
-    diagonals) to use in a banded_solver making calculations quite fast.
 
     Args:
         data_likelihood (Callable): function that calculates the likelihood of
             the data, given the provided means
         theta (float): second parameter of the likelihood of the data
-        alpha (np.ndarray): means of the GMRF
-        Q (sparse.dia_matrix): precision matrix of the GMRF
-        Q_det (float): determinant of the precision matrix of the GRMF
+        gmrf_likelihood (Callable): function that calculates the density of the
+            GMRF (that corresponds to a Multivariate Normal Distribution)
         theta_dist (Callable): probability density function of the distribution
             of theta
         gaus_approx_mean (np.ndarray): mean of the Gaussian approximation to the
             full conditional of the GMRF.
-        gaus_approx_Q (sparse.dia_matrix): precision matrix of the Gaussian 
-            approximation to the full conditional of the GMRF.
+        gaus_approx_det (float): determinant of the precision matrix of the 
+            Gaussian approximation to the full conditional of the GMRF.
 
     Returns:
         float: approximation of the (natural) log probability of the marginal
             posterior of theta. 
     """
     
-    dim = alpha.shape[0]
     x = gaus_approx_mean
+    dim = x.shape[0]
 
     # ln p(y|x, theta)
     likelihood = data_likelihood(x)
 
     # ln p(x|theta)
-    exponent = -(1/2) * (x - alpha) @ Q @ (x - alpha)[:, np.newaxis]
-    gmrf_prior = np.sqrt(Q_det * ((2*np.pi)**(-dim))) * (np.exp(exponent))
-    gmrf_prior = np.log(gmrf_prior)[0]
+    gmrf_prior = np.log(gmrf_likelihood(x))
     
     # ln p(theta)
     theta_prior = np.log(theta_dist(theta))
 
     # ln p_G(x|y, theta)
-    ga_det = general_determinant(gaus_approx_Q)
-    ga_full_conditional_x = np.log(np.sqrt(ga_det * ((2*np.pi)**(-dim))))
+    ga_full_conditional_x = np.sqrt(gaus_approx_det * ((2*np.pi)**(-dim)))
+    ga_full_conditional_x = np.log(ga_full_conditional_x)
 
     # ln p(theta | y)
     return likelihood + gmrf_prior + theta_prior - ga_full_conditional_x
